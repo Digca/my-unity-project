dde0a6a98902d84f8a5a2cfc99e36811c4affa31
Damir Mukatov <damikomu@gmail.com>
2024-02-21 23:23:22 +0600

    Added comment

--- Assets/New Folder/NewBehaviourScript.cs
+++ Assets/New Folder/NewBehaviourScript.cs
@@ -7,6 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //trying to create basic movement
         
     }
 
