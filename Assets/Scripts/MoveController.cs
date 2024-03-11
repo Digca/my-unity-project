83758c1707e83ba56cc69a3095d46fe65ebca69d
Digca <kirill.vdv@mail.ru>
2024-03-11 07:54:18 +0600

    simple move controller and fog

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed = 2f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float moveInput = Input.GetAxis("Horizontal");
+        Vector3 moveDirection = new Vector2(moveInput, 0f);
+        transform.Translate(moveDirection * speed * Time.deltaTime);
     }
 }
